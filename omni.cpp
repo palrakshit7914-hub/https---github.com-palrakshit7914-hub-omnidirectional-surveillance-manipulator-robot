81f6dfcc52653b2f6df86fc6ee9d3c7287bc97c5
Rakshit <palrakshit7914@gmail.com>
2026-04-04 13:19:12 +0530

    Add motor pin definitions for three motors

--- omni.cpp
+++ omni.cpp
@@ -11,3 +11,21 @@ const char* password = "Omni@1234";
 WebServer server(80);
 // CREATE A WEBSOCKET SERVER OBJECT ON PORT 81
 WebSocketsServer webSocket = WebSocketsServer(81);
+
+// Pin definitions
+const int M1_RPWM = 25; // Motor 1 PWM pin
+const int M1_LPWM = 26; // Motor 1 direction pin
+const int M1_REN = 33;  // Motor 1 enable pin
+const int M1_LEN = 32; // Motor 1 direction pin
+
+const int M2_RPWM = 27; // Motor 2 PWM pin
+const int M2_LPWM = 14; // Motor 2 direction pin
+const int M2_REN = 12;  // Motor 2 enable pin
+const int M2_LEN = 13; // Motor 2 direction pin
+
+
+const int M3_RPWM = 18; // Motor 3 PWM pin
+const int M3_LPWM = 19; // Motor 3 direction pin
+const int M3_REN = 5;  // Motor 3 enable pin
+const int M3_LEN = 17; // Motor 3 direction pin
+
