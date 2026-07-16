6225bdeb1b02e6a5eea13cd9d9cca3e99ef41f6a
Rakshit <palrakshit7914@gmail.com>
2026-07-16 16:28:06 +0530

    Refactor voice command prompts for clarity and consistency

--- voice_control.py
+++ voice_control.py
@@ -14,6 +14,10 @@ engine = pyttsx3.init()
 engine.setProperty('rate',160)
 engine.setProperty('volume',1.0)
 
+# ------------------ this is for female voice-------------
+# voices = engine.getProperty('voices')
+# engine.setProperty('voice', voices[1].id)  # index 1 is usually the built-in female voice
+
 def robot_talk(text):
     """Now What cmd u give it will speak"""
     print(f"{text}")
@@ -25,4 +29,14 @@ def capture_voice():
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
         print("I a Listing Sir..." \
-        "whats ur command")
+        "whats ur command sir??")
+        engine.say("I a Listing Sir..." \
+        "whats ur command sir??")
+        recognizer.adjust_for_ambient_noise(source, duration=0.8)
+        audio = recognizer.listen(source)
+
+
+
+if __name__ == "__main__":
+    # Say hello at startup
+    robot_talk("Your Robot is online. Awaiting command")
